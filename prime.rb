a0c8a36c651e0a1d7e081fd0914275e2e012e791
chague530 <chague530@gmail.com>
2017-02-23 16:26:24 -0800

    submit

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(n)
+  if n <= 1 || n.even?
+    return false
+  else
+    i = n / 2
+
+    while i >= 2
+      return false if n % i == 0
+      i -= 1
+    end
+  end
+
+  true
+end
